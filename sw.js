9a4745da3938c7c9ba69437d9385a91ee4beff5e
Gerson Ferreira <gersonfer@gmail.com>
2025-11-06 10:01:41 -0300

    Atualiza PWA para Render (carousel mobile + WS novo)

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
+const CACHE_NAME = "esm-live-cache-v2";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("esm-live-cache").then(cache => {
-      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./sw.js"
+      ]);
     })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
