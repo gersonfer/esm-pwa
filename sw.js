aaeb2d825ff462f37585b30b5caa3003ba0ca5cd
Gerson Ferreira <gersonfer@gmail.com>
2025-11-06 00:05:59 -0300

    ESM Live PWA

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-// Service Worker do ESM Live Viewer
-const CACHE_NAME = "esm-live-cache-v4";
-const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json"];
-
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  e.waitUntil(
+    caches.open("esm-live-cache").then(cache => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    })
+  );
 });
 
 self.addEventListener("fetch", e => {
